65569f917852e08d167657df8b6c69bef9ddb9f3
Reesy <James.rees@outlook.com>
2021-09-17 02:28:54 +0100

    Fixed an issue caused by 'false' being truthy in javascript, wasted a few hours but read a lot about react and learnt a few things, so its a win from a loss, moved websocket initialization outside of the class

--- browser-client/src/BroadcastListener.tsx
+++ browser-client/src/BroadcastListener.tsx
@@ -1,50 +1,47 @@
 import React from 'react';
 import './BroadcastListener.css';
 
+const websocket: WebSocket = new WebSocket('ws://localhost:7070');
+
 class BroadcastListener extends React.Component<any, any> 
 {
-   websocket: WebSocket = new WebSocket('ws://192.168.45.163:7070');
-
-
-   state  = 
+    constructor(props: any)
     {
-        testBool: false,
-        connectedPeersNumber: 0
-    };
+        super(props);
+        this.sendMessage = this.sendMessage.bind(this);
 
-    componentDidMount()
-    {
-
-        this.websocket.onopen = (event: any) =>
+        websocket.onopen = (event: Event) =>
         {
             console.log('Websocket opened');
         };
 
-        this.websocket.onclose = (event: any) =>
+        websocket.onclose = (event: CloseEvent) =>
         {
             console.log('Websocket closed');
         };
-
-        this.websocket.onmessage = (event: any) =>
+    
+        websocket.onmessage = (event: MessageEvent) =>
         {
-            console.log('This was called');
-            // this.setState((state: any) => {
-            //     console.log('Attempting to test state, but I think the screen renderes first');
-            //     state.testBool = true;
-            // });
+            console.log('Recieved: ', event.data);
+            this.setState({ pauseState: event.data });
+        };
+   
+        this.state = 
+        {
+            pauseState: "paused" //this can either be 'paused' or 'playing
         };
 
-    };
-   
-
-    constructor(props: any) {
-        super(props);
-        this.sendMessage = this.sendMessage.bind(this);   
+    }
+    
+    destructor()
+    {
+        websocket.close();
     };
 
     render() {
       return (
-        <div className="Broadcast-laistener">
+        <div>
+    
         <p>
             <button
                 className="Main-button"
@@ -52,14 +49,24 @@ class BroadcastListener extends React.Component<any, any>
                 Click here for something to happen: 
             </button>
         </p>
+     
+         <p> Playback is {this.state.pauseState} </p> 
         </div>
       );
     }
-    
+
     sendMessage()
     {   
-        this.websocket.send("Update");
-        //broadcast a message to all the connected clients    
+        websocket.send("Update");
+        
+        if (this.state.pauseState === "paused")
+        {
+            this.setState({ pauseState: "playing" });
+        }
+        else if (this.state.pauseState === "playing")
+        {
+            this.setState({ pauseState: "paused" });
+        };        
     };
 
 };
--- server/server.ts
+++ server/server.ts
@@ -2,26 +2,23 @@ import WebSocket from 'ws';
 
 const wss : WebSocket.Server = new WebSocket.Server({ port: 7070 });
 
-let booleanState = false;
+let isPaused = true;
 
 wss.on('connection', (ws) =>
 {
   ws.on('message', (message) =>
   {
-     //console.log('received: %s', message);
-    booleanState = !booleanState;
-    console.log('Internal boolean state is: ' + booleanState);
-    // let clientCount = 0;
+    isPaused = !isPaused;
     wss.clients.forEach((client) =>
     {
   
       if (client !== ws && client.readyState === WebSocket.OPEN) 
       {
-        console.log('sending message to server: ');
-        client.send(booleanState.toString());
+        let message = isPaused ? 'paused' : 'playing';
+        console.log('sending message to clients (that didnt trigger the event): ', message);
+        client.send(message);
       };
     });
-    // clientCount = 0;
   });
 
   ws.on('close', () =>
